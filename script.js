4dc7cbf782b0a86068c38a9b0d49b450647816d9
KaiiDesu <exidragt@gmail.com>
2025-02-03 21:33:13 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+function redirectToHome1() {
+    const button = document.getElementById("readyButton");
+    const response = document.getElementById("response");
+
+    response.textContent = "Enjoy bubby!!! ❤️";
+    button.disabled = true;
+    button.style.opacity = "0.7";
+
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+
+    console.log("Redirecting to home1.html..."); // Debugging log
+
+    setTimeout(function() {
+        window.location.href = "one/onepage.html";
+    }, 2000);
+}
+
+function redirectToHome2() {
+    const button = document.getElementById("readyButton");
+    const response = document.getElementById("response");
+
+    response.textContent = "Enjoy bubby!!! ❤️";
+    button.disabled = true;
+    button.style.opacity = "0.7";
+
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+
+    console.log("Redirecting to index.html..."); // Debugging log
+
+    setTimeout(function() {
+        window.location.href = "three/threepage.html";
+    }, 2000);
+}
+
+function redirectToHome3() {
+    const button = document.getElementById("readyButton");
+    const response = document.getElementById("response");
+
+    response.textContent = "Answer me carefully please ❤️";
+    button.disabled = true;
+    button.style.opacity = "0.7";
+
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+
+    console.log("Redirecting to index.html..."); // Debugging log
+
+    setTimeout(function() {
+        window.location.href = "valentine.html";
+    }, 2000);
+}
